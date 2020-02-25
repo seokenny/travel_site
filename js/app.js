3a226dde981e906f3fe3fdddff89072cee6875f7
seokenny <kenn.seo94@gmail.com>
2020-02-25 18:58:13 -0500

    finished base media query

--- js/app.js
+++ js/app.js
@@ -6,7 +6,26 @@ $(".seemore__outer").on("click",function(){
     });
 });
 
+var active = 0;
+
 function onMenuClick() {
     document.getElementById("nav__burger").classList.toggle("change");
     document.getElementById("nav__slide").classList.toggle("active");
+    if(active == 0){
+        document.getElementById("logo_image").src = "images/logoblack.png";
+        active = 1;
+    } else {
+        document.getElementById("logo_image").src = "images/logo.png";
+        active = 0;
+    }
 }
+
+$("#logo_image").mouseover(function(){
+    document.getElementById("logo_image").src = "images/logoblack.png";
+    console.log("asdasd");
+});
+
+$("#logo_image").mouseout(function(){
+    document.getElementById("logo_image").src = "images/logo.png";
+    console.log("asdasd");
+});
